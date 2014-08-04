8bffe95589652e2a2ed79be769efabe754a45c2a
James Patrick Jones <jam@localhost.localdomain>
2014-08-03 22:33:52 -0400

    Experimental

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name voterContactsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the voterContactsApp
+ */
+angular.module('voterContactsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name voterContactsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the voterContactsApp
+ */
+angular.module('voterContactsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
