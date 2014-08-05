3a57ac76b90b55a7fb58d9ff7f422ad1edfbf395
James Patrick Jones <jam@localhost.localdomain>
2014-08-05 00:12:23 -0400

    More tuning

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,33 +1,49 @@
-'use strict';
-
-/**
- * @ngdoc overview
- * @name voterContactsApp
- * @description
- * # voterContactsApp
- *
- * Main module of the application.
- */
-angular
-  .module('voterContactsApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+(function (window, angular, undefined) {
+  'use strict';
+  
+  /**
+   * @ngdoc overview
+   * @name voterContactsApp
+   * @description
+   * # voterContactsApp
+   *
+   * Main module of the application.
+   */
+  angular
+    .module('voterContactsApp', [
+      'ngAnimate',
+      'ngCookies',
+      'ngResource',
+      'ngRoute',
+      'ngSanitize',
+      'ngTouch',
+      'angularLocalStorage'
+    ])
+    .constant('voterAppConstant',{
+      'google': {
+        'init': {
+          clientId: 'CLIENT_ID',
+          apiKey: 'API_KEY'
+        },
+        'oauthCallbackRoute': '/auth'
+      },
+      'applicationResources': {
+          'distGroupMgr': 'https://dev.it.usf.edu/~james/PHP_distGroupMgr/distgroupmgr.php'
+      },
+    })
+    .config(['$routeProvider','voterAppConstant','GooglePlusProvider',function ($routeProvider,voterAppConstant,GooglePlusProvider) {
+      GooglePlusProvider.init(voterAppConstant.google.init);
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/main.html',
+          controller: 'MainCtrl'
+        })
+        .when('/about', {
+          templateUrl: 'views/about.html',
+          controller: 'AboutCtrl'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    }]);
+})(window, window.angular);
