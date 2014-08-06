04cb56228548470a89fa5804d48c0783b1bd3654
James Jones <jamjon3@gmail.com>
2014-08-06 17:42:31 -0400

    Fixing this default multi header into a single one

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,6 +39,7 @@
       $httpProvider.defaults.useXDomain = true;
       $httpProvider.defaults.withCredentials = true;
       delete $httpProvider.defaults.headers.common['X-Requested-With'];
+      $httpProvider.defaults.headers.common.Accept = 'application/json';
       $routeProvider
         .when('/', {
           templateUrl: 'views/main.html',
